3ed360c08064256f536d9f58518ec74801b9b113
Jammo89 <ostalzovmichail@gmail.com>
2023-08-26 19:35:18 +0300

    add delete refactor

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,68 @@
-import express, {json} from 'express'
+import express, { Request, Response } from 'express'
+import assert from "assert";
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 
 const jsonBodyMiddlewaew = express.json()
 app.use(jsonBodyMiddlewaew)
 
 
-const db = {
-    courses: [
-        { id: 1, title: 'front-end' },
-        { id: 2, title: 'bac-end' },
-        { id: 3, title: 'devops' },
-        { id: 4, title: 'oop' }
-    ]
-}
+const products = [{ id: 1, title: 'tomate' }, { id: 2, title: 'orenge' }]
+const adresses = [{ id: 1, value: 'Gorkoko 73' }, { id: 2, value: 'Mira 4' }]
 
-app.get('/', (req, res) => {
-    res.json({ mess: "hello" })
-})
-
-app.get('/curses', (req, res) => {
-    const foundCourses = db.courses
-    
-    if (req.query.title){
-        foundCourses.filter(c => c.title.indexOf(req.query.title as string) > -1 )
-        res.json(foundCourses)
+app.get('/products', (req: Request, res: Response) => {
+    if (req.query.title) {
+        let serchString = req.query.title.toString()
+        res.json(products.filter(p => p.title.indexOf(serchString) > - 1))
     }
-    res.json(foundCourses)
-    
+    res.json(products)
 })
 
-app.get('/curses/:id', (req, res) => {
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    const productTitle = req.params.productTitle
+    const product = products.find(p => p.title === productTitle)
+    if (product) {
+        res.json(product)
 
-    const foundCours = db.courses.find(c => c.id === +req.params.id)
-
-    if (!foundCours) {
+    }
+    else {
         res.sendStatus(404)
-        return
     }
-
-    res.json(foundCours)
-
 })
 
-app.post('/curses', (req,res) => {
-  const newCours = {
-        id: +(new Date()),
-        title: req.body.title
+app.get('/adresses', (req: Request, res: Response) => {
+
+    if (req.query.value) {
+        let serchString = req.query.value.toString()
+        res.json(adresses.filter(p => p.value.indexOf(serchString) > - 1))
     }
-    
-    db.courses.push(newCours)
-    
-    res.json(newCours)
-   
+    res.json(adresses)
 })
 
-app.delete('/curses/:id', (req, res) => {
+app.get('/adresses/:id', (req: Request, res: Response) => {
 
-    db.courses = db.courses.filter(c => c.id !== +req.params.id)
-
-    res.sendStatus(204)
-
+    const adress = adresses.find(p => p.id === +req.params.id)
+    if (adress) {
+        res.json(adress)
+    }
+    else {
+        res.sendStatus(404)
+    }
 })
+
+app.delete('/adresses/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < adresses.length; i++) {
+        if (adresses[i].id === +req.params.id) {
+            adresses.splice(i, 1);
+            res.status(204)
+            return;
+        }
+    }
+    res.status(404)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
